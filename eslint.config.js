df82e906173be3c256659fed6e93627c227d7192
vinyl810 <19thsummer@kaist.ac.kr>
2024-08-20 23:37:20 +0900

    chore: edit some lint configs

--- eslint.config.js
+++ eslint.config.js
@@ -18,24 +18,13 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      // '@stylistic': {
-      //   rules: {
-      //     'object-curly-spacing': ['error', 'always'],
-      //     'no-trailing-spaces': 'error',
-      //     'space-infix-ops': ['error', { int32Hint: false }],
-      //     'no-multi-spaces': 'error',
-      //     'comma-spacing': ['error', { before: false, after: true }],
-      //     'quotes': ['error', 'single'],
-      //     'indent': ['error', 2],
-      //     'arrow-spacing': ['error', { before: true, after: true }],
-      //   },
-      // },
       'stylistic': stylistic,
       'tailwindcss': tailwindcss,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...tailwindcss.configs.recommended.rules,
+      'tailwindcss/no-custom-classname': 'off',
       'object-curly-spacing': ['error', 'always'],
       'no-trailing-spaces': 'error',
       'space-infix-ops': ['error', { int32Hint: false }],
